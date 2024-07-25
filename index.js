942569e1ed9107d3093199c33e2f4b16e0dd611e
Ayesha-p <64060103+Ayesha-p@users.noreply.github.com>
2024-07-25 15:33:02 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,11 @@
+import inquirer from "inquirer";
+const answer = await inquirer.prompt([
+    { message: "Please guess a number between 1 to 10", name: "userGuessedNumber", type: "number" }
+]);
+const randomNumber = Math.floor(Math.random() * 10 + 1);
+if (answer.userGuessedNumber === randomNumber) {
+    console.log("Congtratulations! you guessed right number ");
+}
+else {
+    console.log("Sorry! you guessed wrong number ");
+}
